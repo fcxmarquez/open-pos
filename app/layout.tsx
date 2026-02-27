ab93095fe36c7e4b07f4045e88132e26ab8b5790
fcxmarquez <francisco.marquez.solt@gmail.com>
2026-02-26 20:23:26 -0600

    refactor(typography): remove duplicated body font utility

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body
-        className={`${inter.variable} ${plusJakartaSans.variable} font-body antialiased`}
-      >
+      <body className={`${inter.variable} ${plusJakartaSans.variable} antialiased`}>
         <IOSViewportFix />
         <QueryProvider>
           {children}
